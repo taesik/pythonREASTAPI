d101e5cf6c48d731e708b4e76da49992db061d17
taesik mun <taesic12@knou.ac.kr>
2023-08-07 14:14:36 +0900

    models

--- djangoAPI/employeeapp/models.py
+++ djangoAPI/employeeapp/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+from django.db import models as m
+
 
 # Create your models here.
+class Departments(m.Model):
+	DepartmentId = m.AutoField(primary_key=True)
+	DepartmentName = m.CharField(max_length=300)
+
+
+class Employees(m.Model):
+	EmployeeId = m.AutoField(primary_key=True)
+	EmployeeName = m.CharField(max_length=500)
+	Department = m.CharField(max_length=500)
+	DateOfJoining = m.DateTimeField()
+	PhotoFileName = m.CharField(max_length=500)
+
