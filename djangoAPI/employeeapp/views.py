aaf0b4bb2fe4465958bbe68c109c0ebe2b9942ca
taesik mun <taesic12@knou.ac.kr>
2023-08-07 17:44:38 +0900

    csrf and implementing GET

--- djangoAPI/employeeapp/views.py
+++ djangoAPI/employeeapp/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from django.http.response import JsonResponse
+
+from .models import Departments,Employees
+from .serializers import DptSerializer,EmplySerializer
+
 
 # Create your views here.
+@csrf_exempt
+def departmentApi(req, id=0):
+	if req.method=='GET':
+			departments = Departments.objects.all()
+			departments_serializer = DptSerializer(departments,many=True)
+			return JsonResponse(departments_serializer.data,safe=False)
